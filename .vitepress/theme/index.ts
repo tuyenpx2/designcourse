f44e6dbaed032d7b1fa26e34d7d4eb990cbe1c0b
Pham Xuan Tuyen <tuyen-ph@jrcnet.co.jp>
2026-03-24 18:56:03 +0900

    fix error

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './custom.css'
-// @ts-ignore
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default DefaultTheme
